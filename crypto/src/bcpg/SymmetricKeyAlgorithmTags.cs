1ad426dc2b9795747cebf2651bc5ef102fb64a46
AFGNCAAdminP <94894089+AFGNCAAdminP@users.noreply.github.com>
2023-03-19 21:45:40 +0800

    Move comments to XML documentation

--- crypto/src/bcpg/SymmetricKeyAlgorithmTags.cs
+++ crypto/src/bcpg/SymmetricKeyAlgorithmTags.cs
@@ -1,23 +1,52 @@
 namespace Org.BouncyCastle.Bcpg
 {
-    /**
-    * Basic tags for symmetric key algorithms
-    */
+
+    /// <summary>Basic tags for symmetric key algorithms</summary>
     public enum SymmetricKeyAlgorithmTag
     {
-        Null = 0,         // Plaintext or unencrypted data
-        Idea = 1,         // IDEA [IDEA]
-        TripleDes = 2,    // Triple-DES (DES-EDE, as per spec -168 bit key derived from 192)
-        Cast5 = 3,        // Cast5 (128 bit key, as per RFC 2144)
-        Blowfish = 4,     // Blowfish (128 bit key, 16 rounds) [Blowfish]
-        Safer = 5,        // Safer-SK128 (13 rounds) [Safer]
-        Des = 6,          // Reserved for DES/SK
-        Aes128 = 7,       // Reserved for AES with 128-bit key
-        Aes192 = 8,       // Reserved for AES with 192-bit key
-        Aes256 = 9,       // Reserved for AES with 256-bit key
-        Twofish = 10,     // Reserved for Twofish
-        Camellia128 = 11, // Reserved for AES with 128-bit key
-        Camellia192 = 12, // Reserved for AES with 192-bit key
-        Camellia256 = 13  // Reserved for AES with 256-bit key
+        /// <summary>Plaintext or unencrypted data.</summary>
+        Null = 0,
+
+        /// <summary>International Data Encryption Algorithm (IDEA).</summary>
+        Idea = 1,
+
+        /// <summary>TripleDES (DES-EDE, 168 bit key derived from 192).</summary>
+        TripleDes = 2,
+
+        /// <summary>CAST5 (128 bit key, as per RFC 2144).</summary>
+        Cast5 = 3,
+
+        /// <summary>Blowfish (128 bit key, 16 rounds).</summary>
+        /// <seealso href="https://www.schneier.com/academic/blowfish/">The Blowfish Encryption Algorithm</seealso>
+        Blowfish = 4,
+
+        /// <summary>Secure And Fast Encryption Routine, Strengthened Key schedule, 128-bits (SAFER SK-128) (13 rounds).</summary>
+        Safer = 5,
+
+        /// <summary>Data Encryption Standard, Strengthened Key schedule (DES/SK).</summary>
+        /// <seealso href="https://www.cs.columbia.edu/~smb/papers/ides.pdf">A Better Key Schedule for DES-like Ciphers</seealso>
+        Des = 6,
+
+        /// <summary>Advanced Encryption Standard (AES) with 128-bit key.</summary>
+        Aes128 = 7,
+
+        /// <summary>Advanced Encryption Standard (AES) with 192-bit key.</summary>
+        Aes192 = 8,
+
+        /// <summary>Advanced Encryption Standard (AES) with 256-bit key.</summary>
+        Aes256 = 9,
+
+        /// <summary>Twofish with 256-bit key.</summary>
+        /// <seealso href="https://www.schneier.com/academic/twofish/">Twofish</seealso>
+        Twofish = 10,
+
+        /// <summary>Camellia with 128-bit key.</summary>
+        Camellia128 = 11,
+
+        /// <summary>Camellia with 192-bit key.</summary>
+        Camellia192 = 12,
+
+        /// <summary>Camellia with 256-bit key.</summary>
+        Camellia256 = 13
     }
 }
